1d7920839cd05b6f9b67f4b9c6751214a2f20f71
yxh1234 <hello_yxh@163.com>
2022-08-12 11:37:59 +0800

    fix: 配色调整

--- docs/marine_conservation/helper.tsx
+++ docs/marine_conservation/helper.tsx
@@ -1,5 +1,6 @@
 const MapConfig = {
   mapType: 'GaodeV1',
+  pitch: 90,
   mapOptions: {
     style: 'light',
     center: [119.481623, 39.068625],
@@ -14,21 +15,33 @@ const LayerConfig = {
   color: {
     field: 'Area',
     value: [
-      'rgb(247, 252, 240)',
-      'rgb(224, 243, 219)',
-      'rgb(204, 235, 197)',
-      'rgb(168, 221, 181)',
-      'rgb(123, 204, 196)',
-      'rgb(78, 179, 211)',
-      'rgb(43, 140, 190)',
-      'rgb(8, 104, 172)',
-      'rgb(8, 64, 129)',
+      'rgb(247,252,245)',
+      'rgb(247,252,245)',
+      'rgb(247,252,245)',
+      'rgb(229,245,224)',
+      'rgb(229,245,224)',
+      'rgb(229,245,224)',
+      'rgb(199,233,192)',
+      'rgb(199,233,192)',
+      'rgb(199,233,192)',
+      'rgb(161,217,155)',
+      'rgb(161,217,155)',
+      'rgb(161,217,155)',
+      'rgb(116,196,118)',
+      'rgb(116,196,118)',
+      'rgb(116,196,118)',
+      'rgb(65,171,93)',
+      'rgb(35,139,69)',
+      'rgb(0,109,44)',
+      'rgb(0,68,27)',
     ],
     scale: { type: 'quantile' },
   },
   size: {
     field: 'Area',
-    value: [0, 2, 4, 6, 10, 16],
+    value: ({ Area }) => {
+      return 4 + Number(Area) / 1000;
+    },
   },
   label: {
     field: 'ChineseName',
@@ -39,7 +52,7 @@ const LayerConfig = {
     active: true,
   },
   style: {
-    opacity: 0.9,
+    opacity: 0.8,
     stroke: '#fff',
     strokeWidth: 0.2,
   },

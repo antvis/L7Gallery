758ce8a33e916a01ee9545c4191221b8c9410533
a123495 <2548066918@qq.com>
2022-08-29 12:44:23 +0800

    修改纽约公交路径

--- docs/gallery/taxiTrips.tsx
+++ docs/gallery/taxiTrips.tsx
@@ -1,41 +1,62 @@
-import { LarkMap, LarkMapProps, LineLayer } from '@antv/larkmap';
+import { LarkMap, LarkMapProps, LineLayer, PointLayer } from '@antv/larkmap';
 import React, { useEffect, useState } from 'react';
 
 const config = {
   mapOptions: {
     style: 'dark',
     center: [-73.993896, 40.75011],
-    zoom: 6,
-    rotation: 20,
+    zoom: 8,
+    // rotation: 20,
+    pitch: 120,
   },
 };
 
-const layerStyle = {
+const lineLayerStyle = {
   shape: 'arc3d',
-  color: '#a13ece',
-  size: {
-    field: 'passenger_count',
-    value: [1, 3],
-  },
-  animate: {
-    interval: 1,
-    trailLength: 1,
-    duration: 2,
-  },
   state: {
     active: true,
   },
+  size: 1,
   blend: 'normal',
+  style: {
+    opacity: 0.5,
+    segmentNumber: 15,
+    sourceColor: '#278188', // 起点颜色
+    targetColor: '#3b1eb1', // 终点颜色
+  },
+  zIndex: 3,
+};
+
+const pickupPointLayerStyle = {
+  shape: 'circle',
+  state: {
+    active: true,
+  },
+  size: 2,
+  color: '#2773bd',
+  blend: 'normal',
+
+  zIndex: 2,
+};
+
+const dropoffPointLayerStyle = {
+  shape: 'circle',
+  state: {
+    active: true,
+  },
+  size: 3,
+  color: '#22409a',
+  blend: 'additive',
+  zIndex: 2,
 };
 
 export default () => {
   const [source, setSource] = useState('');
   useEffect(() => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/166c95f5-7d02-4586-a9a1-ee470e1e9689.csv')
+    fetch('https://gw.alipayobjects.com/os/bmw-prod/4d3e5c6e-d3d6-410e-92bc-2e3fe6745a24.csv')
       .then((res) => res.text())
       .then((data) => setSource(data));
   }, []);
-  console.log(source);
   return (
     <LarkMap {...(config as LarkMapProps)} style={{ height: '500px' }}>
       <LineLayer
@@ -49,7 +70,29 @@ export default () => {
             x1: 'dropoff_longitude',
           },
         }}
-        {...layerStyle}
+        {...lineLayerStyle}
+      />
+      <PointLayer
+        source={{
+          data: source,
+          parser: {
+            type: 'csv',
+            y: 'pickup_latitude',
+            x: 'pickup_longitude',
+          },
+        }}
+        {...pickupPointLayerStyle}
+      />
+      <PointLayer
+        source={{
+          data: source,
+          parser: {
+            type: 'csv',
+            y: 'dropoff_latitude',
+            x: 'dropoff_longitude',
+          },
+        }}
+        {...dropoffPointLayerStyle}
       />
     </LarkMap>
   );
